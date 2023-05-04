d02f13e7d54580309e8b61eaa28dd53f6749ea3c
wpcodevo <ziddah.edem@gmail.com>
2023-05-04 06:46:31 +0000

    updated

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,10 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
 import Header from "@/components/header.component";
 
 export default async function Profile() {
   const session = await getServerSession(authOptions);
   const user = session?.user;
-  console.log("user: ", user);
 
   return (
     <>
