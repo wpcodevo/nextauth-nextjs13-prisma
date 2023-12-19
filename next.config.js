094d62349ab6f6ca425e79647ef30a7c25925f93
Edem <contact@codevoweb.com>
2023-12-19 10:26:28 +0000

    updated dependencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"]
-  },
 }
 
 module.exports = nextConfig
